07a9acf74af42dfeab98e1b629b6365da3d17ac3
amperstand <amperstrand@localhost>
2024-01-11 21:07:26 +0100

    add global datastore

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -5,10 +5,17 @@ import connexion
 from swagger_server import encoder
 
 
+# Global in-memory data store
+data_store = {}
+
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Fiatlink FLS01'}, pythonic_params=True)
+
+    # Enable pretty-printing of JSON responses
+    app.app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True 
+
     app.run(port=8080)
 
 
